901e276448f04ccfb8d9243bceeb462aa80b4207
YoujungSon <andn1026@gmail.com>
2023-04-13 17:06:26 +0900

    ✨ #2 feat: hashtag 삭제 기능추가

--- src/components/pages/PostProject/HashTag.tsx
+++ src/components/pages/PostProject/HashTag.tsx
@@ -1,7 +1,8 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useState, useRef } from 'react';
 import styled from 'styled-components';
 
 const HashTag = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [hashtagText, setHashtagText] = useState('');
   const [hashtagList, setHashtagList] = useState<string[]>([]);
 
@@ -17,18 +18,39 @@ const HashTag = () => {
 
   const AddHashtagList = () => {
     setHashtagList([...hashtagList, hashtagText]);
+    const inputEl = inputRef.current;
+    if (inputEl) {
+      inputEl.value = '';
+    }
+  };
+
+  const deleteHashtagList = (index: number) => {
+    const newList = [...hashtagList];
+    newList.splice(index, 1);
+    setHashtagList(newList);
   };
 
   return (
     <HashtagContainer>
       <h3>해시태그</h3>
       <Row>
-        <HashtagInput type="text" onChange={HashtagOnchange} onKeyDown={handleKeyDown} />
+        <HashtagInput type="text" onChange={HashtagOnchange} onKeyDown={handleKeyDown} ref={inputRef} />
         <HashtagBtn onClick={AddHashtagList}>+</HashtagBtn>
       </Row>
       <HashtagList>
-        {hashtagList?.map((hashtag: string) => {
-          return <Hashtag>#{hashtag}</Hashtag>;
+        {hashtagList?.map((hashtag: string, index: number) => {
+          return (
+            <Hashtag key={hashtag}>
+              #{hashtag}{' '}
+              <button
+                onClick={() => {
+                  deleteHashtagList(index);
+                }}
+              >
+                x
+              </button>
+            </Hashtag>
+          );
         })}
       </HashtagList>
     </HashtagContainer>
