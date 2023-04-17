1b9f6ca53d523062b09067f235876173e9d4960c
Seohyun <rmsid1000@gmail.com>
2023-04-17 23:24:07 +0900

    :lipstick: make main UI
    메인페이지 디자인 입히기

--- src/components/pages/Home/HighView.tsx
+++ src/components/pages/Home/HighView.tsx
@@ -5,7 +5,7 @@ import { Splide, SplideSlide } from '@splidejs/react-splide';
 import assets from '@assets';
 import '@splidejs/react-splide/css/skyblue';
 
-type Props = { };
+type Props = {};
 
 const HighView = (props: Props) => {
   const list = [
@@ -41,10 +41,10 @@ const HighView = (props: Props) => {
   return (
     <Container>
       <Head>
-        <div>
+        <ProjectTitle>
           <div>주목받는 프로젝트</div>
           <p>개발자 사이에서 가장 인기있는 프로젝트에 참여해보세요.</p>
-        </div>
+        </ProjectTitle>
         <div>더보기</div>
       </Head>
       <Splide
@@ -58,33 +58,71 @@ const HighView = (props: Props) => {
       >
         {list.map((v, i) => (
           <SplideSlide>
-            <div>
-              <div>{v.title}</div>
-              <div>{v.desc}</div>
-              <div>
-                <span>{v.tag1}</span>
-                <span>{v.tag2}</span>
-              </div>
-              <div>
-                <span>
+            <ProjectBox>
+              <ProjectTop>
+                <p>
+                  <span>프론트엔드</span>
+                  <span>백엔드</span>
+                </p>
+                <p>하뚜</p>
+              </ProjectTop>
+              <ProjectMid>
+                <div>{v.title}</div>
+                <div>{v.desc}</div>
+              </ProjectMid>
+              <ProjectTag>
+                <span>#{v.tag1}</span>
+                <span>#{v.tag2}</span>
+              </ProjectTag>
+              <ProjectBottom>
+                <p>
+                  <span></span>
                   {v.maker.name}({v.maker.roket}KM)
-                </span>
-                <span>{v.like}</span>
-                <span>{v.view}</span>
-              </div>
-            </div>
+                </p>
+                <p>
+                  <span>하트</span>
+                  <span>{v.like}</span>
+                  <span>눈</span>
+                  <span>{v.view}</span>
+                </p>
+              </ProjectBottom>
+            </ProjectBox>
           </SplideSlide>
         ))}
 
-        <SplideSlide>
-          <img src={assets.angry} alt="Image 1" style={{ width: 200 }} />
-        </SplideSlide>
-        <SplideSlide>
-          <img src={assets.angry} alt="Image 2" style={{ width: 200 }} />
-        </SplideSlide>
-        <SplideSlide>
-          <img src={assets.angry} alt="Image 2" style={{ width: 200 }} />
-        </SplideSlide>
+        {list.map((v, i) => (
+          <SplideSlide>
+            <ProjectBox>
+              <ProjectTop>
+                <p>
+                  <span>프론트엔드</span>
+                  <span>백엔드</span>
+                </p>
+                <p>하뚜</p>
+              </ProjectTop>
+              <ProjectMid>
+                <div>{v.title}</div>
+                <div>{v.desc}</div>
+              </ProjectMid>
+              <ProjectTag>
+                <span>#{v.tag1}</span>
+                <span>#{v.tag2}</span>
+              </ProjectTag>
+              <ProjectBottom>
+                <p>
+                  <span></span>
+                  {v.maker.name}({v.maker.roket}KM)
+                </p>
+                <p>
+                  <span>하트</span>
+                  <span>{v.like}</span>
+                  <span>눈</span>
+                  <span>{v.view}</span>
+                </p>
+              </ProjectBottom>
+            </ProjectBox>
+          </SplideSlide>
+        ))}
       </Splide>
     </Container>
   );
@@ -94,15 +132,96 @@ export default HighView;
 
 const Container = styled.div`
   width: 100%;
-  height: 300px;
-  background-color: aquamarine;
+  /* height: 300px; */
+  /* background-color: #ffffff; */
+  /* margin: 0 3% */
+  width: 95%;
 `;
 
 const Head = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+  margin-bottom: 20px;
   div {
     text-align: left;
+  }
+`;
+const ProjectTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  div {
+    font-size: 18px;
+    font-weight: bold;
+  }
+`;
+
+const ProjectBox = styled.div`
+  height: 100%;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border: 2px solid gray;
+  border-radius: 10px;
+  padding: 4%;
+  gap: 15px;
+`;
+
+const ProjectTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 12px;
+  align-items: center;
+  p:nth-child(1) {
+    gap: 5px;
+    display: flex;
+    span {
+      color: blue;
+      border: 1px solid blue;
+      padding: 5px;
+      border-radius: 5px;
+    }
+  }
+`;
+
+const ProjectMid = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  div:nth-child(1) {
+    font-size: 18px;
+  }
+  div:nth-child(2) {
+    font-size: 14px;
+  }
+`;
+
+const ProjectTag = styled.div`
+  display: flex;
+  font-size: 12px;
+  gap: 5px;
+`;
+
+const ProjectBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 12px;
+  p:nth-child(2) {
+    display: flex;
+    gap: 5px;
+  }
+  p:nth-child(1) {
+    display: flex;
+    gap: 5px;
+    align-items: center;
+    span {
+      width: 20px;
+      height: 20px;
+      background-color: gray;
+      border-radius: 10px;
+    }
   }
 `;
