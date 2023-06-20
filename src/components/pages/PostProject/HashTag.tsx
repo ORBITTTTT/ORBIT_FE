a9d8c37dff81eed0ae27652a953536fcab3fc995
YoujungSon <andn1026@gmail.com>
2023-06-20 22:09:46 +0900

    💄 update: 프로젝트 생성페이지

--- src/components/pages/PostProject/HashTag.tsx
+++ src/components/pages/PostProject/HashTag.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, useState, useRef } from 'react';
 import styled from 'styled-components';
-
+import plus from '@assets/postProject/add_circle.svg';
+import close from '@assets/postProject/close.svg';
 const HashTag = () => {
   const inputRef = useRef<HTMLInputElement>(null);
   const [hashtagText, setHashtagText] = useState('');
@@ -32,22 +33,34 @@ const HashTag = () => {
 
   return (
     <HashtagContainer>
-      <h3>해시태그</h3>
+      <HashTitleWrap>
+        <h3>해시태그</h3>
+        <span>최대 20개까지 태그가능</span>
+      </HashTitleWrap>
+
       <Row>
-        <HashtagInput type="text" onChange={HashtagOnchange} onKeyDown={handleKeyDown} ref={inputRef} />
-        <HashtagBtn onClick={AddHashtagList}>+</HashtagBtn>
+        <HashtagInput
+          type="text"
+          onChange={HashtagOnchange}
+          onKeyDown={handleKeyDown}
+          ref={inputRef}
+          placeholder="프로그램을 입력 해주세요"
+        />
+        <HashtagBtn onClick={AddHashtagList}>
+          <img src={plus} alt="플러스아이콘" />
+        </HashtagBtn>
       </Row>
       <HashtagList>
         {hashtagList?.map((hashtag: string, index: number) => {
           return (
             <Hashtag key={hashtag}>
-              #{hashtag}{' '}
+              {hashtag}{' '}
               <button
                 onClick={() => {
                   deleteHashtagList(index);
                 }}
               >
-                x
+                <img src={close} alt="닫기아이콘" />
               </button>
             </Hashtag>
           );
@@ -65,28 +78,59 @@ const HashtagContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
+  h3 {
+    font-weight: 600;
+    font-size: 19px;
+  }
+  span {
+    font-size: 13px;
+    line-height: 18px;
+    color: #7e7e7e;
+  }
 `;
 
+const HashTitleWrap = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 9px;
+  margin-bottom: 28px;
+  margin-top: 56px;
+`;
 const Row = styled.div`
   display: flex;
+  position: relative;
+  width: 216px;
+  margin-bottom: 12px;
 `;
 const HashtagInput = styled.input`
-  border: 1px solid #222;
-  border-radius: 5px 0px 0px 5px;
-  padding: 5px 10px;
-  border-right: none;
+  padding: 9px 16px;
+  background: #ffffff;
+  border: 1.7px solid #e1e1e1;
+  border-radius: 35px;
+  font-weight: 500;
+  font-size: 14px;
+  outline: none;
 `;
 const HashtagBtn = styled.button`
-  border: 1px solid #222;
-  border-radius: 0px 5px 5px 0px;
-  padding: 5px;
+  position: absolute;
+  right: 15px;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 24px;
+  height: 24px;
 `;
 const HashtagList = styled.ul`
   display: flex;
   gap: 10px;
 `;
 const Hashtag = styled.li`
-  border: 1px solid #222;
-  border-radius: 5px;
-  padding: 5px 10px;
+  background: #1560fb;
+  border-radius: 35px;
+  padding: 8px 16px;
+  color: #fff;
+  font-weight: 500;
+  font-size: 14px;
+  button {
+    margin-left: 8px;
+  }
 `;
--- src/components/pages/PostProject/InputNumber.tsx
+++ src/components/pages/PostProject/InputNumber.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const InputNumber = ({ career }: any) => {
+const InputNumber = () => {
+  const careerList = ['프론트엔드', '백엔드', '디자이너', 'PM'];
+
   const [value, setValue]: any = useState(0);
   const handleIncrement = () => {
     if (value !== 10) {
@@ -15,24 +17,28 @@ const InputNumber = ({ career }: any) => {
     }
   };
   return (
-    <ProjectNumberContainer key={career}>
-      <ProjectInfoLabel htmlFor={`${career}Number`}>{career}</ProjectInfoLabel>
-      <ProjectNumber>
-        <PlusNumberBtn onClick={handleDecrement}>-</PlusNumberBtn>
-        <ProjectInfoInput
-          type="number"
-          value={value}
-          min={0}
-          max={10}
-          step={1}
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
-          placeholder="0"
-          id={`${career}Number`}
-        />
-        <MinusNumberBtn onClick={handleIncrement}>+</MinusNumberBtn>
-      </ProjectNumber>
+    <ProjectNumberContainer>
+      {careerList.map((career: string) => (
+        <ProjectNumberWrap>
+          <ProjectInfoLabel htmlFor={`${career}Number`}>{career}</ProjectInfoLabel>
+          <ProjectNumber>
+            <PlusNumberBtn onClick={handleDecrement}>-</PlusNumberBtn>
+            <ProjectInfoInput
+              type="number"
+              value={value}
+              min={0}
+              max={10}
+              step={1}
+              onChange={(e) => {
+                setValue(e.target.value);
+              }}
+              placeholder="0"
+              id={`${career}Number`}
+            />
+            <MinusNumberBtn onClick={handleIncrement}>+</MinusNumberBtn>
+          </ProjectNumber>
+        </ProjectNumberWrap>
+      ))}
     </ProjectNumberContainer>
   );
 };
@@ -40,9 +46,16 @@ const InputNumber = ({ career }: any) => {
 export default InputNumber;
 
 const ProjectNumberContainer = styled.div`
+  width: 45%;
   display: flex;
-  justify-content: space-between;
-  gap: 10px;
+  flex-wrap: wrap;
+  gap: 42px 0;
+`;
+const ProjectNumberWrap = styled.div`
+  width: 265px;
+  gap: 64px;
+  display: flex;
+  align-items: center;
 `;
 const ProjectNumber = styled.div`
   position: relative;
@@ -53,26 +66,39 @@ const ProjectNumber = styled.div`
 `;
 
 const ProjectInfoInput = styled.input`
-  padding: 0 15px;
   text-align: center;
+  width: 57px;
+  border: 0;
+  padding-left: 15px;
+  font-weight: 500;
+  font-size: 20px;
+  outline: none;
 `;
 const ProjectInfoLabel = styled.label`
-  margin-right: 10px;
+  width: 78px;
 `;
 
 const PlusNumberBtn = styled.button`
-  width: 15px;
+  width: 30px;
+  height: 30px;
+  color: #fff;
+  background: #1560fb;
+  border-radius: 2px;
   text-align: center;
   position: absolute;
-  left: 0;
+  left: -30px;
   top: 50%;
   transform: translateY(-50%);
 `;
 const MinusNumberBtn = styled.button`
-  width: 15px;
+  width: 30px;
+  height: 30px;
+  color: #fff;
+  background: #1560fb;
+  border-radius: 2px;
   text-align: center;
   position: absolute;
-  right: 0;
+  right: -30px;
   top: 50%;
   transform: translateY(-50%);
 `;
